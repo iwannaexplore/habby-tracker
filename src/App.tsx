3db41e1d2b4b50be843c5d40a7269b9712be29db
dmitriysenko <dmitriy.senko.job@gmai.com>
2022-08-17 11:22:45 +0300

    main design is created

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
  let week = ["M", "T", "W", "T", "F", "S", "S"];
  let monthWeekDayNames = week.concat(week.concat(week.concat(week)));
  let monthDaysNumber = Array.from(Array(28).keys());
- let emptyFields = Array.from({length: 30}, (_, i) => <FontAwesomeIcon icon={solid("check")}/>)
+ let emptyFields = Array.from({length: 29}, (_, i) => <FontAwesomeIcon icon={solid("check")}/>)
  return (
   <>
    <header className={"header"}>
@@ -57,58 +57,25 @@ function App() {
        <div className={"goal__col__label center__flex__content"}>Goal</div>
       </td>
       <td rowSpan={2} className={"without__border"}>
-       <div className={"archive__col__label center__flex__content"}>Archive</div>
+       <div className={"archive__col__label center__flex__content"}>Archived</div>
       </td>
      </tr>
      <tr>
-      {monthDaysNumber.map((e) => <td><div className={"month__day center__flex__content"}>{e}</div></td>)}
+      {monthDaysNumber.map((e) => <td>
+       <div className={"month__day center__flex__content"}>{e}</div>
+      </td>)}
      </tr>
      <tr>
       <td className={"habits__col__name"}>Eat pussy</td>
-      {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-one"}>{e}</div></td>)}
+      {emptyFields.map((e) => <td>
+       <div className={"default__cell center__flex__content cell-color-one"}>{e}</div>
+      </td>)}
+      <td>12</td>
      </tr>
-     <tr>
-      <td className={"habits__col__name"}>Suck balls</td>
-      {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-two"}>{e}</div></td>)}
-     </tr>
-     <tr>
-      <td className={"habits__col__name"}>Work hard</td>
-      {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-three"}>{e}</div></td>)}
-     </tr>
-     <tr>
-     <td className={"habits__col__name"}>Eat pussy</td>
-     {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-four"}>{e}</div></td>)}
-    </tr>
-     <tr>
-      <td className={"habits__col__name"}>Suck balls</td>
-      {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-five"}>{e}</div></td>)}
-     </tr>
-     <tr>
-      <td className={"habits__col__name"}>Work hard</td>
-      {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-six"}>{e}</div></td>)}
-     </tr>
-     <tr>
-      <td className={"habits__col__name"}>Work hard</td>
-      {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-seven"}>{e}</div></td>)}
-     </tr>
-     <tr>
-      <td className={"habits__col__name"}>Work hard</td>
-      {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-eight"}>{e}</div></td>)}
-     </tr>
-     <tr>
-      <td className={"habits__col__name"}>Work hard</td>
-      {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-nine"}>{e}</div></td>)}
-     </tr>
-     <tr>
-      <td className={"habits__col__name"}>Work hard</td>
-      {emptyFields.map((e) => <td><div className={"default__cell center__flex__content cell-color-ten"}>{e}</div></td>)}
-     </tr>
-
     </table>
-
    </section>
    <footer>
-    pussy
+    work hard
    </footer>
   </>
  );
